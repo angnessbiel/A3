69b326054971031a88f7a4c16f0eb8d9d7fa092b
lucasrodriguess-ads <lucas.rodrigues.fern@gmail.com>
2025-11-21 21:46:29 -0300

    Atualizando o banco de dados com .gitignore

--- A3/src/DAO/Connect.java
+++ A3/src/DAO/Connect.java
@@ -2,20 +2,27 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
+import java.io.FileInputStream;
 
 public class Connect {
 
     public static Connection getConnection() {
-
         try {
+            // Carregar driver
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/estoque_db?useSSL=false&serverTimezone=UTC",
-                "root",
-                "Mel32593599?"
-            );
+            // Carregar configurações externas
+            Properties props = new Properties();
+            FileInputStream file = new FileInputStream("config.properties");
+            props.load(file);
 
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
+            // Conectar
+            Connection conn = DriverManager.getConnection(url, user, password);
             return conn;
 
         } catch (Exception e) {
