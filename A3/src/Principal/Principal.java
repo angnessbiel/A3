7c3672cc7e40180bc87f5738836e49ce28f899e0
gabriel <gabriel.mendesangnes@gmail.com>
2025-12-01 18:04:07 -0300

    estilização

--- A3/src/Principal/Principal.java
+++ A3/src/Principal/Principal.java
@@ -1,12 +1,23 @@
 package Principal;
 
 import View.TelaLogin;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import java.awt.Font;
+import javax.swing.UIManager;
 
 public class Principal {
 
     public static void main(String[] args) {
+
+        try {
+            FlatMacLightLaf.setup();
+        } catch (Exception ex) {
+            System.err.println("Falha ao iniciar o LaF: " + ex.getMessage());
+        }
+        
+        UIManager.put("defaultFont", new Font("SansSerif", Font.PLAIN, 13));
+
         TelaLogin objetotela = new TelaLogin();
         objetotela.setVisible(true);
     }
-    
 }
